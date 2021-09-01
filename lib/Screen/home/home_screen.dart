f5aa948382d20faf0cb1f6d518271b089c92ac44
Pinkal6191 <patelpinkal0691@gmail.com>
2021-09-02 00:01:35 +0530

    left menu

--- lib/Screen/home/home_screen.dart
+++ lib/Screen/home/home_screen.dart
@@ -2,10 +2,13 @@ import 'package:danini/BuildAppToolBar.dart';
 import 'package:danini/Screen/home/components/body.dart';
 import 'package:flutter/material.dart';
 
+import '../../NavDrawer.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: BuildAppToolBar(context, "", false),
       body: Body(),
     );
